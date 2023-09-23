5f45340e195dc3532ffec9f56a7ec0f6a9f0f5cc
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-23 22:23:29 +0530

    Started working on #page4

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-function loco(){
+function loco() {
   gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 }
-loco()
+loco();
 
 var clutter = "";
-document.querySelector("#page2>h2").textContent.split(" ").forEach(function(dets){
-  clutter += `<span> ${dets} </span>`
-  document.querySelector("#page2>h2").innerHTML = clutter;
-})
+document
+  .querySelector("#page2>h2")
+  .textContent.split(" ")
+  .forEach(function (dets) {
+    clutter += `<span> ${dets} </span>`;
+    document.querySelector("#page2>h2").innerHTML = clutter;
+  });
 
-
-gsap.to("#page2>h2>span",{
-  ScrollTrigger:{
+gsap.to("#page2>h2>span", {
+  ScrollTrigger: {
     trigger: `#page2>h2.span`,
-    start:`top bottom`,
-    end:`bottom top`,
-    scroller:`#main`,
-    scrub:.3,
-    
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: 0.3,
   },
-  stagger:.1,
-  color:`white`
-})
-
-
-
+  stagger: 0.1,
+  color: `white`,
+});
 
 function canvas() {
   const canvas = document.querySelector("#page3>canvas");
   const context = canvas.getContext("2d");
-  
+
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  
-  
+
   window.addEventListener("resize", function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     render();
   });
-  
+
   function files(index) {
     var data = `
     
@@ -198,40 +201,39 @@ function canvas() {
    `;
     return data.split("\n")[index];
   }
-  
+
   const frameCount = 121;
-  
+
   const images = [];
   const imageSeq = {
     frame: 1,
   };
-  
+
   for (let i = 0; i < frameCount; i++) {
     const img = new Image();
     img.src = files(i);
     images.push(img);
   }
-   // Function to update the animation frame
-   function updateFrame() {
+  // Function to update the animation frame
+  function updateFrame() {
     imageSeq.frame++;
     if (imageSeq.frame >= frameCount) {
       imageSeq.frame = 0; // Reset to the first frame to create a loop
     }
     render();
   }
-  
+
   // Set an interval to update the frame every 100 milliseconds
   const animationInterval = setInterval(updateFrame, 100);
-  
+
   images[1].onload = render;
-  
+
   function render() {
     context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
     scaleImage(images[imageSeq.frame], context);
   }
-  
-  function scaleImage(img, ctx) {
 
+  function scaleImage(img, ctx) {
     var canvas = ctx.canvas;
     var hRatio = canvas.width / img.width;
     var vRatio = canvas.height / img.height;
@@ -251,18 +253,27 @@ function canvas() {
       img.height * ratio
     );
   }
-    // ... (your image scaling logic)
-  }
-  
-  // Scroll-triggered pinning
-  ScrollTrigger.create({
-    trigger: "#page3",
-    pin: true,
-    start: "top top",
-    end: "250% top",
-    scroller: "#main",
-  });
-  canvas()
+  // ... (your image scaling logic)
+}
+
+// Scroll-triggered pinning
+ScrollTrigger.create({
+  trigger: "#page3",
+  pin: true,
+  start: "top top",
+  end: "250% top",
+  scroller: "#main",
+});
+canvas();
+
+
+
+
+
+
+// ----------------------------------------------------------------------------------------------
+                                    // Used for different approch
+// ----------------------------------------------------------------------------------------------
 
 //   gsap.to(imageSeq, {
 //     frame: frameCount - 1,
@@ -278,13 +289,13 @@ function canvas() {
 //     },
 //     onUpdate: render,
 //   });
-  
+
 //   images[1].onload = render;
-  
+
 //   function render() {
 //     scaleImage(images[imageSeq.frame], context);
 //   }
-  
+
 //   function scaleImage(img, ctx) {
 //     var canvas = ctx.canvas;
 //     var hRatio = canvas.width / img.width;
@@ -306,7 +317,7 @@ function canvas() {
 //     );
 //   }
 //   ScrollTrigger.create({
-  
+
 //     trigger: "#page3" ,
 //     pin: true,
 //     // markers:true,
@@ -316,8 +327,3 @@ function canvas() {
 //     end: `250% top`,
 //   });
 // }
-
-
-
-
-
